2979b3ff90f44d7f11d57af509c68e63c7b9069e
Mohammed Sakri <mohammed.sakri.ai.ds@gmail.com>
2026-07-13 17:49:11 +0530

    Add Q9 Total Salary solution

---
+++ 01_basics_and_variables/q9_total_salary.py
@@ -0,0 +1,6 @@
+basic_salary = int(input("Enter basic salary: "))
+bonus = int(input("Enter bonus: "))
+allowance = int(input("Enter allowance: "))
+total_salary = basic_salary + bonus + allowance
+
+print(f"Total Salary: {total_salary:.2f}")
